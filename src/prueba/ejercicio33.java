f0cd9876529d0b00600090a3dcc87ae11f280094
Your Name <cielo.ma07@gmail.com>
2024-05-24 12:08:33 -0300

    subido viernes

--- src/prueba/ejercicio33.java
+++ src/prueba/ejercicio33.java
@@ -1,14 +1,24 @@
 package prueba;
 
+import java.util.Scanner;
+
 public class ejercicio33 {
 public static void main(String[] args) {
+	Scanner dato = new Scanner(System.in);
 	
+	String [] nombre = new String [11];
+	int []  edad = new int [11];
 	
-	for(int jugador= 0;  jugador<=11; jugador++) {
+	for(int i= 0;  i<=10; i++) {
 		
 		System.out.println("Ingrese su nombre: ");
-		
+		nombre[i] =  dato.next();
 		System.out.println("Ingrese su edad: ");
+		edad[i] = dato.nextInt();
 	}
-}
+	for(int i= 0;  i<=10; i++) {
+		System.out.println("Mostrar todos los nombres: "+ nombre[i]);
+		System.out.println("mostrar todas las edades: "+ edad[i]);
+	}
+	}
 }
