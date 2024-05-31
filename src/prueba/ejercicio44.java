9b05af2d3b33ea8d2c33cc6f0be3dc20d4baf981
Your Name <cielo.ma07@gmail.com>
2024-05-31 12:07:03 -0300

    31-05

---
+++ src/prueba/ejercicio44.java
@@ -0,0 +1,31 @@
+package prueba;
+
+import java.util.Scanner;
+
+public class ejercicio44 {
+	
+	public static void main(String[] args) {
+		Scanner dato = new Scanner(System.in);
+
+		int [] dni = new int[4];
+		
+		for (int i = 0; i < dni.length; i++) {
+			
+			System.out.println("ingrese el numero de dni: ");
+			dni[i] = dato.nextInt();
+		
+			
+			if(dni[i] %2 == 0) {
+				System.out.println(dni);
+				
+			}
+		
+		
+		
+			
+		}
+		
+		
+	}
+
+}
